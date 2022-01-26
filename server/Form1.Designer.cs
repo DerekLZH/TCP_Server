d16826f0106cb5d58062b3fdfd5f68860660fb58
547585862@qq.com <001046@df-global.cn>
2022-01-26 12:28:18 +0100

    upload project file for the first time

---
+++ server/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnGetLocalIP = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnServerConn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IPAddress";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(70, 18);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(306, 21);
+            this.txtIP.TabIndex = 1;
+            // 
+            // btnGetLocalIP
+            // 
+            this.btnGetLocalIP.Location = new System.Drawing.Point(400, 16);
+            this.btnGetLocalIP.Name = "btnGetLocalIP";
+            this.btnGetLocalIP.Size = new System.Drawing.Size(92, 23);
+            this.btnGetLocalIP.TabIndex = 2;
+            this.btnGetLocalIP.Text = "GetIP";
+            this.btnGetLocalIP.UseVisualStyleBackColor = true;
+            this.btnGetLocalIP.Click += new System.EventHandler(this.btnGetLocalIP_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LocalPort";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(70, 67);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(304, 21);
+            this.txtPort.TabIndex = 4;
+            // 
+            // btnServerConn
+            // 
+            this.btnServerConn.Location = new System.Drawing.Point(400, 65);
+            this.btnServerConn.Name = "btnServerConn";
+            this.btnServerConn.Size = new System.Drawing.Size(92, 23);
+            this.btnServerConn.TabIndex = 5;
+            this.btnServerConn.Text = "StartServer";
+            this.btnServerConn.UseVisualStyleBackColor = true;
+            this.btnServerConn.Click += new System.EventHandler(this.btnServerConn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Chat";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(66, 128);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(309, 188);
+            this.txtMsg.TabIndex = 7;
+            this.txtMsg.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Server";
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(75, 362);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(298, 21);
+            this.txtSendMsg.TabIndex = 10;
+            this.txtSendMsg.TextChanged += new System.EventHandler(this.txtSendMsg_TextChanged);
+            this.txtSendMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSendMsg_KeyDown_1);
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Location = new System.Drawing.Point(417, 360);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(74, 22);
+            this.btnSendMsg.TabIndex = 11;
+            this.btnSendMsg.Text = "Send";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 411);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnServerConn);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGetLocalIP);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnGetLocalIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnServerConn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Button btnSendMsg;
+    }
+}
+
---
+++ server/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        Thread threadWatch = null;
+        Socket socketWatch = null;
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnServerConn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ipaddress = IPAddress.Parse(this.txtIP.Text.Trim());
+                IPEndPoint endpoint = new IPEndPoint(ipaddress, int.Parse(this.txtPort.Text.Trim()));
+
+                socketWatch.Bind(endpoint);
+                socketWatch.Listen(20);
+                threadWatch = new Thread(WatchConnecting);
+                threadWatch.IsBackground = true;
+                threadWatch.Start();
+                txtMsg.AppendText("start listening from Client..." + "\r\n");
+                this.btnServerConn.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                txtMsg.AppendText("start Server failure!" + "\r\n");
+                this.btnServerConn.Enabled = true;
+            }
+        }
+
+        Socket socConnection = null;
+
+        
+       private void WatchConnecting()
+        {
+            while (true)
+            {
+                socConnection = socketWatch.Accept();
+                txtMsg.AppendText("connect to Client successfully!" + "\r\n");
+               /* ParameterizedThreadStart pts = new ParameterizedThreadStart(ServerRecMsg);
+                Thread thr = new Thread(pts);*/
+                Thread thr = new Thread(ServerRecMsg);
+                thr.IsBackground = true;
+                thr.Start(socConnection);
+            }
+        }
+
+        private void ServerSendMsg(String sendMsg)
+        {
+            try
+            {
+                byte[] arrSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+                socConnection.Send(arrSendMsg);
+                txtMsg.AppendText("Server"+"\t" + GetCurrentTime() + "\r\n" + sendMsg + "\r\n");
+            }
+            catch (Exception ex)
+            {
+                txtMsg.AppendText("Client is disconnected, cannot send message!" + "\r\n");
+            }
+        }
+
+        private void ServerRecMsg(object socketClientPara)
+        {
+            Socket socketServer = socketClientPara as Socket;
+            while (true)
+            {
+                byte[] arrServerRecMsg = new byte[1024 * 1024];
+                try
+                {
+                    int length = socketServer.Receive(arrServerRecMsg);
+                    string strSRecMsg = Encoding.UTF8.GetString(arrServerRecMsg, 0, length);
+                    txtMsg.AppendText("Client"+"\t" + GetCurrentTime() + "\r\n" + strSRecMsg + "\r\n");
+                }
+                catch (Exception ex)
+                {
+                    txtMsg.AppendText("Client is disconnected!" + "\r\n");
+                    break;
+                }
+            }
+        }
+
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            ServerSendMsg(this.txtSendMsg.Text.Trim());
+            this.txtSendMsg.Clear();
+        }
+
+        private void txtSendMsg_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private DateTime GetCurrentTime()
+        {
+            DateTime currentTime = new DateTime();
+            currentTime = DateTime.Now;
+            return currentTime;
+        }
+
+        public IPAddress GetLocalIPv4Address()
+        {
+            IPAddress localIpv4 = null;
+            IPAddress[] IpList = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress IP in IpList)
+            {
+                if (IP.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIpv4 = IP;
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return localIpv4;
+        }
+
+        private void btnGetLocalIP_Click(object sender, EventArgs e)
+        {
+            IPAddress localIP = GetLocalIPv4Address();
+            this.txtIP.Text = localIP.ToString();
+
+        }
+
+       
+
+        private void txtSendMsg_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ServerSendMsg(txtSendMsg.Text.Trim());
+                this.txtSendMsg.Clear();
+            }
+        }
+    }
+}
